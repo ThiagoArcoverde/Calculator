52e104564c8c68a57b454fa21f50da99d5f7ea4e
Thiago Arcoverde <tluiz2701@gmail.com>
2022-11-09 20:08:45 -0300

    Initial commit

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿
+namespace Calculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelResult = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.butNum0 = new System.Windows.Forms.Button();
+            this.butNum1 = new System.Windows.Forms.Button();
+            this.butNum2 = new System.Windows.Forms.Button();
+            this.butNum3 = new System.Windows.Forms.Button();
+            this.butNum4 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.panelResult.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelResult
+            // 
+            this.panelResult.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panelResult.Controls.Add(this.labelResult);
+            this.panelResult.Controls.Add(this.panel1);
+            this.panelResult.Location = new System.Drawing.Point(-2, -2);
+            this.panelResult.Name = "panelResult";
+            this.panelResult.Size = new System.Drawing.Size(385, 95);
+            this.panelResult.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Location = new System.Drawing.Point(0, 94);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(385, 461);
+            this.panel1.TabIndex = 1;
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Font = new System.Drawing.Font("Lucida Fax", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelResult.Location = new System.Drawing.Point(14, 11);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(124, 39);
+            this.labelResult.TabIndex = 2;
+            this.labelResult.Text = "Result";
+            this.labelResult.Click += new System.EventHandler(this.labelResult_Click);
+            // 
+            // butNum0
+            // 
+            this.butNum0.Location = new System.Drawing.Point(50, 460);
+            this.butNum0.Name = "butNum0";
+            this.butNum0.Size = new System.Drawing.Size(75, 40);
+            this.butNum0.TabIndex = 1;
+            this.butNum0.Text = "0";
+            this.butNum0.UseVisualStyleBackColor = true;
+            // 
+            // butNum1
+            // 
+            this.butNum1.Location = new System.Drawing.Point(50, 385);
+            this.butNum1.Name = "butNum1";
+            this.butNum1.Size = new System.Drawing.Size(75, 40);
+            this.butNum1.TabIndex = 2;
+            this.butNum1.Text = "1";
+            this.butNum1.UseVisualStyleBackColor = true;
+            // 
+            // butNum2
+            // 
+            this.butNum2.Location = new System.Drawing.Point(160, 385);
+            this.butNum2.Name = "butNum2";
+            this.butNum2.Size = new System.Drawing.Size(75, 40);
+            this.butNum2.TabIndex = 3;
+            this.butNum2.Text = "2";
+            this.butNum2.UseVisualStyleBackColor = true;
+            // 
+            // butNum3
+            // 
+            this.butNum3.Location = new System.Drawing.Point(270, 385);
+            this.butNum3.Name = "butNum3";
+            this.butNum3.Size = new System.Drawing.Size(75, 40);
+            this.butNum3.TabIndex = 4;
+            this.butNum3.Text = "3";
+            this.butNum3.UseVisualStyleBackColor = true;
+            // 
+            // butNum4
+            // 
+            this.butNum4.Location = new System.Drawing.Point(50, 310);
+            this.butNum4.Name = "butNum4";
+            this.butNum4.Size = new System.Drawing.Size(75, 40);
+            this.butNum4.TabIndex = 5;
+            this.butNum4.Text = "4";
+            this.butNum4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(160, 310);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 40);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "5";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(270, 310);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 40);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "6";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(270, 235);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 40);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "9";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(160, 235);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 40);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "8";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(50, 235);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 40);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "7";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GrayText;
+            this.ClientSize = new System.Drawing.Size(382, 553);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.butNum4);
+            this.Controls.Add(this.butNum3);
+            this.Controls.Add(this.butNum2);
+            this.Controls.Add(this.butNum1);
+            this.Controls.Add(this.butNum0);
+            this.Controls.Add(this.panelResult);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.panelResult.ResumeLayout(false);
+            this.panelResult.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelResult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Button butNum0;
+        private System.Windows.Forms.Button butNum1;
+        private System.Windows.Forms.Button butNum2;
+        private System.Windows.Forms.Button butNum3;
+        private System.Windows.Forms.Button butNum4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
